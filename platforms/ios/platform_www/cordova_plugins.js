75bf9c603d88da850f039112f4024d2ab9535edb
Michael <mihui@cn.ibm.com>
2016-08-06 01:56:45 +0800

    Test only commit

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/WatsonSpeechPlugin/www/watson-sdk.js",
+        "id": "WatsonSpeechPlugin.WatsonSDK",
+        "pluginId": "WatsonSpeechPlugin",
+        "clobbers": [
+            "window.WatsonSDK"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "WatsonSpeechPlugin": "1.0.0"
+}
+// BOTTOM OF METADATA
+});
